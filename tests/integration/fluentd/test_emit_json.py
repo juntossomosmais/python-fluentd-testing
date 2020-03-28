76704a85f12ee60fe05bf0e91236129dcc6639a4
Willian Antunes <willian.lima.antunes@gmail.com>
2020-03-28 17:49:29 -0300

    Clearer fixture name

--- tests/integration/fluentd/test_emit_json.py
+++ tests/integration/fluentd/test_emit_json.py
@@ -4,13 +4,13 @@ from tests.resources.support import absolute_path_fluentd_output_file
 
 
 @pytest.fixture
-def setup_fluentd_test():
+def setup_fluentd_scenario_1():
     folder_location, abs_file_path = absolute_path_fluentd_output_file("fluentd-test-output-1.log")
     with FluentdEvaluator("fluent-1.conf", folder_location, abs_file_path).initialize_fluent_daemon() as f_evaluator:
         yield f_evaluator
 
 
-def test_should_emit_and_check_if_log_matches(setup_fluentd_test):
+def test_should_emit_and_check_if_log_matches(setup_fluentd_scenario_1):
     emitted = {"log": "jafar", "tag": "jsm.something"}
 
-    assert setup_fluentd_test.emit_it_and_check_if_matches_with(emitted)
+    assert setup_fluentd_scenario_1.emit_it_and_check_if_matches_with(emitted)
