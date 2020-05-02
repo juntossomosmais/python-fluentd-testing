d84d912bfac81e045c5f473b82979b61e2171b8c
Willian Antunes <willian.lima.antunes@gmail.com>
2020-05-02 14:33:01 -0300

    Ignore health check messages
    This can be pretty annoying in your K8S cluster

--- tests/integration/fluentd/test_emit_json.py
+++ tests/integration/fluentd/test_emit_json.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from python_fluentd_testing.fluentd_evaluator import FluentdEvaluator
 from tests.resources.support import absolute_path_fluentd_output_file
@@ -31,6 +33,14 @@ def setup_fluentd_scenario_3():
 def setup_fluentd_scenario_4():
     folder_location, abs_file_path = absolute_path_fluentd_output_file("fluentd-test-output-4.log")
     fluentd_evaluator = FluentdEvaluator("fluent-4.conf", folder_location, abs_file_path, 24227)
+    with fluentd_evaluator.initialize_fluent_daemon() as f_evaluator:
+        yield f_evaluator
+
+
+@pytest.fixture
+def setup_fluentd_scenario_5():
+    folder_location, abs_file_path = absolute_path_fluentd_output_file("fluentd-test-output-5.log")
+    fluentd_evaluator = FluentdEvaluator("fluent-5.conf", folder_location, abs_file_path, 24228)
     with fluentd_evaluator.initialize_fluent_daemon() as f_evaluator:
         yield f_evaluator
 
@@ -121,3 +131,51 @@ def test_should_emit_and_delete_some_keys(setup_fluentd_scenario_4):
     cleaned_result = try_to_remove_key_otherwise_return_it(result, "date", "tag")
     del message["kubernetes"]["something"]
     assert cleaned_result == message
+
+
+def test_should_not_accept_health_check(setup_fluentd_scenario_5):
+    message_1 = {
+        "log": '{"levelname": "INFO", "asctime": "2020-04-24 19:00:49,878", "request_id": "646e161e-5f98-43a3-a369-693c0112999a", "name": "gunicorn.access", "message": "GET /healthcheck HTTP/1.1", "http_status": 200, "ip_address": "10.130.81.220", "response_length": "3", "referer": "-", "user_agent": "kube-probe/1.14+", "request_time": 0.000896, "date": "[24/Apr/2020:19:00:49 -0300]"}',
+        "stream": "stderr",
+        "docker": {"container_id": "b486c8e1725ae522953b6eb5df7ac0a3e0a8316912ab682f7804af6df2302ae9"},
+        "kubernetes": {
+            "container_name": "chumaco",
+            "namespace_name": "production",
+            "pod_name": "chumaco-deployment-5bdd6d6d58-chq95",
+            "pod_id": "ebe5ff7c-70ad-11ea-b1ce-02c121ebda79",
+            "labels": {"app": "chumaco", "pod-template-hash": "5bdd6d6d58"},
+            "host": "ip-127-0-0-2.ec2.internal",
+            "master_url": "https://127.0.0.1:443/api",
+            "namespace_id": "c02f189a-9f2a-11e9-879d-0ad999a381fc",
+        },
+        "@timestamp": "2020-04-24T22:00:50.742098210+00:00",
+    }
+    message_2 = {
+        "log": '{"levelname": "INFO", "asctime": "2020-04-24 19:00:49,878", "request_id": "646e161e-5f98-43a3-a369-693c0112999a", "name": "gunicorn.access", "message": "POST /healthcheck HTTP/1.1", "http_status": 200, "ip_address": "10.130.81.220", "response_length": "3", "referer": "-", "user_agent": "kube-probe/1.14+", "request_time": 0.000896, "date": "[24/Apr/2020:19:00:49 -0300]"}',
+        "stream": "stderr",
+        "docker": {"container_id": "b486c8e1725ae522953b6eb5df7ac0a3e0a8316912ab682f7804af6df2302ae9"},
+        "kubernetes": {
+            "container_name": "chumaco",
+            "namespace_name": "production",
+            "pod_name": "chumaco-deployment-5bdd6d6d58-chq95",
+            "pod_id": "ebe5ff7c-70ad-11ea-b1ce-02c121ebda79",
+            "labels": {"app": "chumaco", "pod-template-hash": "5bdd6d6d58"},
+            "host": "ip-127-0-0-2.ec2.internal",
+            "master_url": "https://127.0.0.1:443/api",
+            "namespace_id": "c02f189a-9f2a-11e9-879d-0ad999a381fc",
+        },
+        "@timestamp": "2020-04-24T22:00:50.742098210+00:00",
+    }
+
+    setup_fluentd_scenario_5.emit_it(message_1)
+    result = setup_fluentd_scenario_5.emit_it_and_get_computed_result(message_2)
+
+    assert result.get("date") is not None
+    assert result.get("tag") is not None
+
+    cleaned_result = try_to_remove_key_otherwise_return_it(result, "date", "tag")
+
+    message_2["application"] = json.loads(message_2["log"])
+    del message_2["kubernetes"]["master_url"]
+    del message_2["kubernetes"]["namespace_id"]
+    assert cleaned_result == message_2
